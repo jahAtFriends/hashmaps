fee1baf70f89aebcd89160bd6dfcd68456b9aa75
Joel Hammer <jhammer@friendsbalt.org>
2023-01-23 08:28:21 -0500

    Create SeparateChainingHM.java

---
+++ SeparateChainingHM.java
@@ -0,0 +1,15 @@
+public class SeparateChainingHM<Key, Value> {
+  
+  public SeparateChainingHM() {
+    this(997);
+  }
+  
+  public SeparateChaingingHM(int M) {
+  }
+  
+  public Value get(Key key) {
+  }
+  
+  public void put(Key key, Value val) {
+  }  
+}
